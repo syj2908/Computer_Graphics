ea1d4d8935986181f98aea6df70c8d06ed0e2315
syj2908 <2512849424@qq.com>
2020-11-08 18:25:02 +0800

    beautify OUTPUT

--- dda.py
+++ dda.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 def ROUND(a):
 	return int(a + 0.5)
 
-def DrawDDA(x1,y1,x2,y2):
+def DrawDDA(x1, y1, x2, y2):
     xs = []
     ys = []
-    x,y = x1,y1
+    x, y = x1, y1
     xs.append(ROUND(x))
     ys.append(ROUND(y))
-    dx = int(x2-x1)
-    dy = int(y2-y1)
+    dx = int(x2 - x1)
+    dy = int(y2 - y1)
     if abs(dx) > abs(dy):
         steps = abs(dx)
     else:
@@ -23,15 +23,15 @@ def DrawDDA(x1,y1,x2,y2):
         xs.append(ROUND(x))
         ys.append(ROUND(y))
     plt.show()
-    plt.axvline(x=0,linewidth=1, color='k')#v x轴
-    plt.axhline(y=0,linewidth=1, color='k')#h y轴
-    plt.axis([-50, 50, -50, 50],'equal')
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.axis('equal')
+    plt.axvline(x=0, linewidth=1, color='k')
+    plt.axhline(y=0, linewidth=1, color='k')
+    #plt.axis([-100, 100, -100, 100])
+    plt.title("DDA algorithm")
+    plt.xlabel('X')
+    plt.ylabel('Y')
     for i in range(0,len(xs)):
-        plt.scatter(xs[i],ys[i],color='k',s=1)
+        plt.scatter(xs[i], ys[i], color='k', s=1)
         plt.pause(0.1)
         plt.draw()
     plt.show()
-
-#DrawDDA(1,3,20,9)
